5d9475551ef1cb77389dd2d97525719e597f8dec
Denys Chmeliuk <chmeliuk@syneforge.com>
2014-03-19 17:21:40 +0200

    remove flag. apply magic :)

--- ipakey/auth.py
+++ ipakey/auth.py
@@ -9,7 +9,7 @@ class IpaAuth(object):
         self.access_key = self.authenticate()
 
     def authenticate(self):
-        gss_flags = kerberos.GSS_C_DELEG_FLAG | kerberos.GSS_C_MUTUAL_FLAG | kerberos.GSS_C_SEQUENCE_FLAG
+        gss_flags = kerberos.GSS_C_MUTUAL_FLAG | kerberos.GSS_C_SEQUENCE_FLAG
         _ignore, context = kerberos.authGSSClientInit('HTTP@' + self.hostname, gssflags=gss_flags)
         _ignore = kerberos.authGSSClientStep(context, '')
         response = kerberos.authGSSClientResponse(context)
