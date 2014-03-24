d0b3c201147a2dd4e30eee46891cdad58a4ed778
Denys Chmeliuk <chmeliuk@syneforge.com>
2014-03-24 11:06:47 +0200

    add kerberos ticket check/create
    kinit with password

--- ipakey/auth.py
+++ ipakey/auth.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import kerberos
+import subprocess
+import settings
 
 
 class IpaAuth(object):
@@ -9,9 +11,23 @@ class IpaAuth(object):
         self.access_key = self.authenticate()
 
     def authenticate(self):
+        if not has_kerberos_ticket():
+            create_kerberos_ticket()
+
         gss_flags = kerberos.GSS_C_MUTUAL_FLAG | kerberos.GSS_C_SEQUENCE_FLAG
         _ignore, context = kerberos.authGSSClientInit('HTTP@' + self.hostname, gssflags=gss_flags)
         _ignore = kerberos.authGSSClientStep(context, '')
         response = kerberos.authGSSClientResponse(context)
 
         return response
+
+
+def has_kerberos_ticket():
+    return True if subprocess.call(['/usr/bin/klist', '-s']) == 0 else False
+
+
+def create_kerberos_ticket():
+    kinit_args = ['/usr/bin/kinit', '%s@%s' % ('admin', 'SF')]
+    kinit = subprocess.Popen(kinit_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    kinit.stdin.write('%s\n' % settings.PASSWORD)
+    kinit.wait()
